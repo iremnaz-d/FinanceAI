b67964efa7b4e0b8a8e712b7d5b4fe4808ee49c4
İrem Naz Durgut <iremnazdurgut4@gmail.com>
2026-07-15 02:26:58 +0300

    Data Cleaning is annoying

--- src/infrastructure/data/excel_parser.py
+++ src/infrastructure/data/excel_parser.py
@@ -8,6 +8,10 @@ class ExcellReader:
     Bankalardan gelen farklı CSV formatlarını okuyan ve
      ham veriyi standart veri sözlüklerine dönüştüren yardımcı sınıf.
     """
+
+    def __init__ (self, path):
+        self.path = path
+
     df = pd.read_excel(r"C:\Users\irem naz\Desktop\FinanceAI\src\data\Transaction_History.xlsx")
     df.drop(index = df.index[:11], inplace = True)
     df.columns = ['Date', 'Transaction ID', 'Description', 'Amount', 'Balance']
